487438e39483beb4caf27dd1fb14fd9714f732bf
aminul171 <131786057+aminul171@users.noreply.github.com>
2024-06-11 00:58:58 +0600

    Create res.js

---
+++ scripts/cmds/res.js
@@ -0,0 +1,39 @@
+ const axios = require('axios');
+
+module.exports = {
+    config: {
+        name: "res",
+        aliases: [],
+        version: "1.0",
+        author: "JISHAN",
+        countDown: 10,
+        role: 2,
+        shortDescription: "Get response from URL",
+        longDescription: "Fetches response data from a provided URL and sends it back.",
+        category: "utility",
+        guide: "{pn} res <url>"
+    },
+
+    onStart: async function ({ message, args }) {
+        const url = args[0];
+        if (!url) {
+            return message.reply("🚫 Please provide a URL.");
+        }
+
+        try {
+            const response = await axios.get(url);
+
+            if (response.headers['content-type'].includes('application/json')) {
+                return message.reply("json\n" + JSON.stringify(response.data, null, 2) + "\n");
+            } else {
+                const streamResponse = await axios.get(url, { responseType: 'stream' });
+                return message.reply({
+                    attachment: streamResponse.data
+                });
+            }
+        } catch (error) {
+            console.error(error);
+            await message.reply("❌ An error occurred while fetching data from the URL.");
+        }
+    }
+};
