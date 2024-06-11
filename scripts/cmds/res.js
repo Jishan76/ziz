503f1347529e1459b55572f04ffff5d5d9167092
aminul171 <131786057+aminul171@users.noreply.github.com>
2024-06-11 20:45:51 +0600

    Update res.js

--- scripts/cmds/res.js
+++ scripts/cmds/res.js
@@ -1,4 +1,7 @@
  const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const FormData = require('form-data');
 
 module.exports = {
     config: {
@@ -16,6 +19,7 @@ module.exports = {
 
     onStart: async function ({ message, args }) {
         const url = args[0];
+        
         if (!url) {
             return message.reply("🚫 Please provide a URL.");
         }
@@ -24,7 +28,17 @@ module.exports = {
             const response = await axios.get(url);
 
             if (response.headers['content-type'].includes('application/json')) {
-                return message.reply(JSON.stringify(response.data, null, 2));
+                return message.reply({
+                    body: JSON.stringify(response.data, null, 2)
+                });
+            } else if (response.headers['content-type'].includes('text/html')) {
+                const txtFileName = 'response.txt';
+                const filePath = path.join(__dirname, txtFileName);
+                fs.writeFileSync(filePath, response.data);
+
+                const responseUrl = await submitTextFromFile(filePath);
+
+                return message.reply(`✅ The HTML content to the following URL: \n\n${responseUrl}`);
             } else {
                 const streamResponse = await axios.get(url, { responseType: 'stream' });
                 return message.reply({
@@ -35,5 +49,30 @@ module.exports = {
             console.error(error);
             await message.reply("❌ An error occurred while fetching data from the URL.");
         }
+    }
+};
+
+const submitTextFromFile = async (filePath) => {
+    try {
+        if (!fs.existsSync(filePath)) {
+            throw new Error('File not found');
+        }
+
+        const form = new FormData();
+        form.append('file', fs.createReadStream(filePath));
+
+        const response = await axios.post('https://aminulzisan.com/zedbin', form, {
+            headers: {
+                ...form.getHeaders()
+            },
+            maxContentLength: Infinity,
+            maxBodyLength: Infinity
+        });
+
+        console.log('URL to access your text:', response.data.url);
+        return response.data.url;
+    } catch (error) {
+        console.error('Error submitting text:', error.message);
+        throw error;
     }
 };
