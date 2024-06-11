a7e589bfc3aebdfec74adb5711f99f429d9d3eab
aminul171 <131786057+aminul171@users.noreply.github.com>
2024-06-11 18:45:24 +0600

    Update checkApproval.js

--- bot/handler/checkApproval.js
+++ bot/handler/checkApproval.js
@@ -10,7 +10,7 @@ async function checkApproval(event, message) {
     // Check if the thread exists and is approved
     if (!thread || !thread.approved) {
         console.log("Thread not approved:", thread);
-        await message.reply("You're not allowed to use this bot. This bot requires Approval.\n\n1. Reply to this message\n2. Contact the admin to get approval.\nCODE: BAKI-24-7-BOT\n\nm.me/jishanahammedII");
+        await message.reply("You're not allowed to use this bot.There are 2 methods to Get Approval.\n\n1. Reply Anything to this message\n2. Contact the admin to get approval.\nCODE: BAKI-24-7-BOT\n\nm.me/jishanahammedII");
         return false; // Return false if not approved
     }
 
