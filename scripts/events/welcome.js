8aac4d9b5190dedcf7c5dd56bf7a8a0b29a24896
aminul171 <131786057+aminul171@users.noreply.github.com>
2024-06-11 21:57:17 +0600

    Update welcome.js

--- scripts/events/welcome.js
+++ scripts/events/welcome.js
@@ -74,7 +74,7 @@ module.exports = {
 							tag: user.fullName,
 							id: user.userFbId
 						});
-						userPositions.push(totalMembers + 1);
+						userPositions.push(formatOrdinal(totalMembers + 1));
 						totalMembers++;
 					}
 
@@ -113,3 +113,9 @@ module.exports = {
 		}
 	}
 };
+
+function formatOrdinal(number) {
+	const suffixes = ["th", "st", "nd", "rd"];
+	const v = number % 100;
+	return number + (suffixes[(v - 20) % 10] || suffixes[v] || suffixes[0]);
+}
